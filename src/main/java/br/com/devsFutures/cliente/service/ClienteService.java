945aec02bfd2b1d31f9878b2f59e9f11e8275077
maddytec <maddytec@gmail.com>
2023-05-19 03:50:45 +0100

    Adicionado os metodos de consulta e exclusao de cliente

--- src/main/java/br/com/devsFutures/cliente/service/ClienteService.java
+++ src/main/java/br/com/devsFutures/cliente/service/ClienteService.java
@@ -1,10 +1,23 @@
 package br.com.devsFutures.cliente.service;
 
 import br.com.devsFutures.cliente.entities.Cliente;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.UUID;
 
 public interface ClienteService {
 
 
      Cliente criar(Cliente cliente);
 
+     Page<Cliente> consultar(Pageable pageable);
+
+     Cliente consultarPorUuid(UUID uuid);
+
+     Cliente consultarPorCpf(String cpf);
+
+     Cliente consultarPorEmail(String email);
+
+     void excluirPorCpf(String cpf);
 }
--- src/main/java/br/com/devsFutures/cliente/service/serviceImpl/ClienteServiceImpl.java
+++ src/main/java/br/com/devsFutures/cliente/service/serviceImpl/ClienteServiceImpl.java
@@ -4,8 +4,12 @@ import br.com.devsFutures.cliente.entities.Cliente;
 import br.com.devsFutures.cliente.repository.ClienteRepository;
 import br.com.devsFutures.cliente.service.ClienteService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -13,8 +17,38 @@ public class ClienteServiceImpl implements ClienteService {
 
 
     private final ClienteRepository clienteRepository;
+
     @Override
     public Cliente criar(Cliente cliente) {
-      return clienteRepository.save(cliente);
+        return clienteRepository.save(cliente);
+    }
+
+    @Override
+    public Page<Cliente> consultar(Pageable pageable) {
+        return clienteRepository.findAll(pageable);
+    }
+
+    @Override
+    public Cliente consultarPorUuid(UUID uuid) {
+        return clienteRepository.findById(uuid)
+                .orElseThrow(() -> new RuntimeException("Cliente não encontrado por uuid: " + uuid));
+    }
+
+    @Override
+    public Cliente consultarPorCpf(String cpf) {
+        return clienteRepository.buscaClientePorDocumento(cpf)
+                .orElseThrow(() -> new RuntimeException("Cliente não encontrado pelo cpf: " + cpf));
+    }
+
+    @Override
+    public Cliente consultarPorEmail(String email) {
+        return clienteRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("Cliente não encontrado por email: " + email));
+    }
+
+    @Override
+    @Transactional
+    public void excluirPorCpf(String cpf) {
+        clienteRepository.deleteByCpf(cpf);
     }
 }
